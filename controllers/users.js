473b6cfd9314437134b91d67d94cfdcd31b9169e
Sammy <samanthabeltran1993@gmail.com>
2024-10-08 15:20:27 -0500

    building signin up

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,48 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs')
+const User = require('../models/user')
 
-router.post('/signup', (req, res) => {
-    res.json({ message: 'Signup route' });
+const SALT_LENGTH = 12;
+
+router.post('/signin', async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.body.username });
+        if (user && bcrypt.compareSync(req.body.password, user.hashedPassword)) {
+            res.json({ message: 'Username and password match!' })  
+        } else {
+            res.json({ message: 'Username or password incorrect, please try again.' })
+        }
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+})
+
+
+router.post('/signup', async (req, res) => {
+  try {
+    const userInDatabase = await User.findOne({ username: req.body.username });
+    if (userInDatabase) {
+        return res.status(400).json
+        ({ error: 'Username already taken' })
+    }
+    const emailInDatabase = await User.findOne({ email:req.body.email });
+    if (emailInDatabase) {
+        return res.status(400).json
+        ({ error: 'Email already in use. Please try a different email, or login.' })
+    }
+    const hashedPassword = bcrypt.hashSync(req.body.password, SALT_LENGTH);
+
+    const user = await User.create({
+        username: req.body.username,
+        email: req.body.email,
+        hashedPassword,
+        role: req.body.role
+    });
+    res.status(201).json({ user })
+  } catch (error) {
+    
+  }
 });
 
 module.exports = router;
--- models/user.js
+++ models/user.js
@@ -1,12 +1,34 @@
 const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
     email: {
         type: String,
-        required: true
+        required: true,
+        unique: true,
+        match: /.+\@.+\..+/
     },
     hashedPassword: {
         type: String,
         required: true
+    },
+    role: {
+        type: String,
+        enum: ['parent', 'caregiver'],
+        required: true
+    },
+    timeStamp: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+userSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        delete returnedObject.hashedPassword;
     }
 });
 
